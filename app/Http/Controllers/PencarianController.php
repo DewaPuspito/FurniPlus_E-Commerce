966868f4ebc3c6bcdb7d32af695ad60c42370d71
rismaintan <rismaintan60@gmail.com>
2023-06-01 12:08:26 +0700

    update pencarian barang byID

--- app/Http/Controllers/PencarianController.php
+++ app/Http/Controllers/PencarianController.php
@@ -8,10 +8,24 @@ use Illuminate\Support\Facades\Http;
 
 class PencarianController extends Controller
 {
-    public function index (){
-        $response = Http::get('http://127.0.0.1:8000/api/data-barang');
+    public function index(){
+        $response = Http::get('http://127.0.0.1:8000/api/data-barang/');
         $data= $response->json();
         return response()->json($data);
-        // return view('index', compact('data'));
     }
+
+    public function show($id_barang){
+        $response = Http::get('http://127.0.0.1:8000/api/data-barang/'.$id_barang);
+        $data= $response->json();
+        if($data) {
+            return response()->json($data);
+        }
+        else {
+            return response()->json([
+                'status' => 404,  
+                'message'=> 'Data Barang Not Found'
+            ]);
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; 
-
+use App\Http\Controllers\PencarianController; 
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/barang',[PencarianController::class,'index']);
+Route::get('/barang/{id_barang}', [PencarianController::class, 'show']); 
 
 Route::group(['middleware'=>'api','prefix'=>'auth'], function($router){
     Route::post('/register',[AuthController::class,'register']);
