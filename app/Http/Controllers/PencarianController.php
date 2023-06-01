faef789812fd9472463df96117d02a124450705b
rismaintan <rismaintan60@gmail.com>
2023-06-01 19:05:35 +0700

    add try n catch

--- app/Http/Controllers/PencarianController.php
+++ app/Http/Controllers/PencarianController.php
@@ -3,28 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\RequestException;
 
 class PencarianController extends Controller
 {
     public function index(){
-        $response = Http::get('http://127.0.0.1:8000/api/data-barang/');
-        $data= $response->json();
-        return response()->json($data);
+        try {
+            $response = Http::get('http://127.0.0.1:8000/api/data-barang/');
+            $data = $response->json();
+            return response()->json($data);
+        } catch (RequestException $e) {
+            return response()->json(['status' => '500', 'message' => "Bad Request"], Response::HTTP_BAD_REQUEST);
+        } catch (\Exception $e) {
+            return response()->json(['status' => '500', 'message' => "Intrnal Service Error"], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function show($id_barang){
-        $response = Http::get('http://127.0.0.1:8000/api/data-barang/'.$id_barang);
-        $data= $response->json();
-        if($data) {
-            return response()->json($data);
-        }
-        else {
-            return response()->json([
-                'status' => 404,  
-                'message'=> 'Data Barang Not Found'
-            ]);
+        try {
+            $response = Http::get('http://127.0.0.1:8000/api/data-barang/'.$id_barang);
+            $data= $response->json();
+            if($data) {
+                return response()->json($data);
+            }
+            else {
+                return response()->json([
+                    'status' => 404,  
+                    'message'=> 'Data Barang Not Found'
+                ]);
+            }
+        } catch (RequestException $e) {
+            return response()->json(['status' => '500', 'message' => "Bad Request"], Response::HTTP_BAD_REQUEST);
+        } catch (\Exception $e) {
+            return response()->json(['status' => '500', 'message' => "Internal Service Error"], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
