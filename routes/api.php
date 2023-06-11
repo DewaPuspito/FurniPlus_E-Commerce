171b851482bd35f42639d03a7d81d9df4ee0d385
DewaPuspito <dewapuspito@student.telkomuniversity.ac.id>
2023-06-11 09:37:19 +0700

    update api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; 
 use App\Http\Controllers\PencarianController; 
+use App\Http\Controllers\DataPesananController;
+use App\Http\Controllers\DataRefundController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,3 +31,5 @@ Route::group(['middleware'=>'api','prefix'=>'auth'], function($router){
     Route::post('/profile_update',[AuthController::class,'profile_update']);
 }); 
 
+Route::apiResource('data_pesanan', DataPesananController::class);
+Route::apiResource('data_refund', DataRefundController::class);
