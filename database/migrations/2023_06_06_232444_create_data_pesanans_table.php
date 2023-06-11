876516bbfef204be51bc7cadfc3956c93da97936
DewaPuspito <dewapuspito@student.telkomuniversity.ac.id>
2023-06-11 09:40:46 +0700

    checkout refund databases

---
+++ database/migrations/2023_06_06_232444_create_data_pesanans_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_barang');
+            $table->unsignedBigInteger('id_user');
+            $table->string('nama_pengguna');
+            $table->string('alamat');
+            $table->string('no_hp');
+            $table->integer('jumlah_pesanan');
+            $table->integer('total_harga');
+            $table->string('status');
+            $table->string('resi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_pesanan');
+    }
+};
